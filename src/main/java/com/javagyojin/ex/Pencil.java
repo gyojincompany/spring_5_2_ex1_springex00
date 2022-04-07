8346a4c0fcf88691ec033c5c0f1a0c909fbf3cca
gyojincompany <luckyred@gmail.com>
2022-04-07 12:45:13 +0900

    스프링 5장 DI활용 연습예제 2

---
+++ src/main/java/com/javagyojin/ex/Pencil.java
@@ -0,0 +1,5 @@
+package com.javagyojin.ex;
+
+public interface Pencil {
+	void use();
+}
---
+++ src/main/java/com/javagyojin/ex/Pencil4B.java
@@ -0,0 +1,12 @@
+package com.javagyojin.ex;
+
+public class Pencil4B implements Pencil {
+
+	@Override
+	public void use() {
+		// TODO Auto-generated method stub
+		System.out.println("4B 굵기로 써집니다!");
+
+	}
+
+}
